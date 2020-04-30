2f14f9f3666602ba8d8cb2a743d8702b6e81c352
Marco <marco.faltoni@outlook.it>
2020-04-30 17:13:28 +0200

    aggiunti commenti all'esercizio completo

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ $('.fa-circle').click(function() {
     // rimuovo momentaneamente la classe bianco ai pallini
     $('.fa-circle').removeClass('bianco');
 
-
     // associo le img trovate con l'indice dei pallini, e gli aggiungo la classe visible ogni volta che clicco sul pallino.
     $('.images').find('img').eq(indice).addClass('visible');
     // associo la classe bianco con l'indice dei pallini, cosi facendo avranno lo sfondo bianco ogni volta che li clicco.
@@ -28,17 +27,24 @@ $('.freccia-destra').click(function() {
     // rimuovo temporaneamente la classe bianco al pallino
     current_bullet.removeClass('bianco');
 
-    // assegno
+    // recupero l'immagine successiva
     var next_image = current_image.next('.images img');
+    // recupero il pallino successivo
     var next_bullet = current_bullet.next('.bullets .fa-circle');
 
+    // verifico che esista l'immagine e il pallino successivo
     if (next_image.length !=0) {
+        // c'è un immagine e un pallino successivo
+        // gli metto la classe visible all'img e la classe bianco al pallino
         next_image.addClass('visible');
         next_bullet.addClass('bianco');
     } else {
+        // non c'è un'immagine o un pallino successivo
+        // riparto dall'inizio assegnando "first child" all'img e al pallino
         next_image = $('.images img:first-child');
         next_image.addClass('visible');
 
+        // abbreviazione del comando sopra
         $('.bullets .fa-circle:first-child').addClass('bianco');
     }
 });
@@ -47,21 +53,31 @@ $('.freccia-destra').click(function() {
 $('.freccia-sinistra').click(function() {
     // recupero l'img che ha la classe visible in questo momento
     var current_image = $('img.visible');
+    // recupero il paòlino che ha la classe bianco in questo momento
     var current_bullet = $('.fa-circle.bianco')
 
+    // rimuovo temporaneamente la classe visible all'immagine
     current_image.removeClass('visible');
+    // rimuovo temporaneamente la classe bianco al pallino
     current_bullet.removeClass('bianco');
 
+    // recupero l'immagine e il pallino precedente
     var next_image = current_image.prev('.images img');
     var next_bullet = current_bullet.prev('.bullets .fa-circle');
 
+    // verifico che esista l'immagine e il pallino precedente
     if (next_image.length !=0) {
+        // c'è un immagine e un pallino precedente
+        // gli metto la classe visible all'img e la classe bianco al pallino
         next_image.addClass('visible');
         next_bullet.addClass('bianco');
     } else {
+        // non c'è un'immagine o un pallino precedente
+        // riparto dall'inizio assegnando "last child" all'img e al pallino
         next_image = $('.images img:last-child');
         next_image.addClass('visible');
 
+        // abbreviazione del comando sopra
         $('.bullets .fa-circle:last-child').addClass('bianco');
     }
 });
