8d21a1bb6a220c8409e379671ce5db060f72b72b
Marco <marco.faltoni@outlook.it>
2020-05-04 15:45:07 +0200

    Automatic SlideShow Integrato correttamente

--- main.js
+++ main.js
@@ -84,24 +84,29 @@ $('.freccia-sinistra').click(function() {
 
 var slideIndex = 0;
 showSlides();
+var timeout;
 
 function showSlides() {
   var i;
   var slides = $('.images img');
   var bullets = $('.bullets .fa-circle');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  slides.removeClass('visible');
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < bullets.length; i++) {
-    bullets[i].className = bullets[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  bullets[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  bullets.removeClass('bianco');
+  slides.eq(slideIndex-1).addClass('visible');
+  bullets.eq(slideIndex-1).addClass('bianco');
+  timeout = setTimeout(showSlides, 2000); // Change image every 2 seconds
 };
 
-$('.fa-pause').click(function() {
-    clearTimeout(showSlides);
+$('.fa-pause').click(function(){
+    clearTimeout(timeout);
+    $('.fa-pause').removeClass('visible');
+    $('.fa-play').addClass('visible');
+});
+
+$('.fa-play').click(function(){
+    showSlides();
+    $('.fa-play').removeClass('visible');
+    $('.fa-pause').addClass('visible');
 });
