dbfcb92b47ff663d2e6d6a18b92ce34f2b192a2f
Marco <marco.faltoni@outlook.it>
2020-05-04 14:52:13 +0200

    Automatic Slideshow

--- main.js
+++ main.js
@@ -81,3 +81,27 @@ $('.freccia-sinistra').click(function() {
         $('.bullets .fa-circle:last-child').addClass('bianco');
     }
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = $('.images img');
+  var bullets = $('.bullets .fa-circle');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < bullets.length; i++) {
+    bullets[i].className = bullets[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  bullets[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+};
+
+$('.fa-pause').click(function() {
+    clearTimeout(showSlides);
+});
